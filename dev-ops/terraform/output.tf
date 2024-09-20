ebbbc7efe50b4263fa0ad29fe5ff5e667537a83a
yzhbankov <yzhbankov@redpointpositioning.com>
2024-09-20 02:05:59 +0000

    Fix secure output

--- dev-ops/terraform/output.tf
+++ dev-ops/terraform/output.tf
@@ -4,20 +4,20 @@ output "aws_region" {
 
 output "iot_certificate_pem" {
   value     = aws_iot_certificate.iot_certificate.certificate_pem
-  sensitive = false
+  sensitive = true
 }
 
 output "iot_certificate_arn" {
   value     = aws_iot_certificate.iot_certificate.arn
-  sensitive = false
+  sensitive = true
 }
 
 output "iot_private_key" {
   value     = aws_iot_certificate.iot_certificate.private_key
-  sensitive = false
+  sensitive = true
 }
 
 output "iot_public_key" {
   value     = aws_iot_certificate.iot_certificate.public_key
-  sensitive = false
+  sensitive = true
 }
