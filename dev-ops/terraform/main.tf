7737b74a0ed09c7d7678255355b4155fe003fa73
yzhbankov <yzhbankov@redpointpositioning.com>
2024-10-09 02:54:55 +0000

    wip

--- dev-ops/terraform/main.tf
+++ dev-ops/terraform/main.tf
@@ -150,14 +150,14 @@ resource "aws_iot_policy" "iot_policy" {
   policy = jsonencode({
     Version = "2012-10-17",
     Statement = [{
-      "Effect": "Allow",
-      "Action": [
+      "Effect" : "Allow",
+      "Action" : [
         "iot:Connect",
         "iot:Publish",
         "iot:Subscribe",
         "iot:Receive"
       ],
-      "Resource": "*"
+      "Resource" : "*"
     }]
   })
 }
